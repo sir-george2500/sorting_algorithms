baf993a01ecc1f3bb81855e25f71f40bb6d86502
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-11-22 18:00:57 +0000

    solution for insertion sort

---
+++ sort.h
@@ -0,0 +1,31 @@
+#ifndef _SORTING_H_
+#define _SORTING_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/** Struct Double Linked List */
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/** Given FUN prototype */
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/** TASK Prototype */
+void insertion_sort_list(listint_t **list);
+
+#endif
